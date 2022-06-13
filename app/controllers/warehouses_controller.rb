af280950eb9fe05f63913abd96cb553b9156dc20
Emma Rünzel <emma@ruenzel.de>
2022-06-13 17:00:24 +0200

    Warehouse Controller

---
+++ app/controllers/warehouses_controller.rb
@@ -0,0 +1,50 @@
+# app/controllers/warehouses_controller.rb
+class WarehousesController < ApplicationController
+  before_action :warehouse_check, only: %i[show edit update destroy]
+
+  def index
+    @warehouses = Warehouse.all
+  end
+
+  def show; end
+
+  def new
+    @warehouse = Warehouse.new
+  end
+
+  def create
+    @warehouse = Warehouse.new(warehouse_params)
+    if @warehouse.save
+      redirect_to @warehouse
+    else
+      render :new
+    end
+  end
+
+  def edit
+    if @warehouse.update(warehouse_params)
+      redirect_to @warehouse, notice: 'Your amazing warehouse is edited!'
+    else
+      render :edit
+    end
+  end
+
+  def update; end
+
+  def destroy
+    @warehouse.destroy
+    redirect_to warehouses_path,
+                status: :see_other,
+                data: { turbo_confirm: 'Are you sure you want to delete this beautiful Warehouse?' }
+  end
+
+  private
+
+  def warehouse_check
+    @warehouse = Warehouse.find(params[:id])
+  end
+
+  def warehouse_params
+    params.require(:warehouse).permit(:name, :address, :capacity)
+  end
+end
