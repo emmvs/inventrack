f2113d3d20e3c21f2a231d39be7b912a210e7c83
Emma Rünzel <emma@ruenzel.de>
2022-06-13 17:05:53 +0200

    Item Controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,52 @@
+# app/controllers/items_controller.rb
 class ItemsController < ApplicationController
+  before_action :item_check, only: %i[show edit update destroy]
+
+  def index
+    @items = Item.all
+  end
+
+  def show; end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @warehouse = Warehouse.find(params[:warehouse_id])
+    @item.warehouse = @warehouse
+    if @item.save
+      redirect_to @item
+    else
+      render :new
+    end
+  end
+
+  def edit
+    if @item.update(item_params)
+      redirect_to @item, notice: 'Your amazing Item is edited!'
+    else
+      render :edit
+    end
+  end
+
+  def update; end
+
+  def destroy
+    @item.destroy
+    redirect_to items_path,
+                status: :see_other,
+                data: { turbo_confirm: 'Are you sure you want to delete this beautiful Item?' }
+  end
+
+  private
+
+  def item_check
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :type_of_item)
+  end
 end
