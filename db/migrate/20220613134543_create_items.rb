1e22dc57639c9af6838f44c85f20f0588435cb84
Emma Rünzel <emma@ruenzel.de>
2022-06-13 16:41:25 +0200

    Migration II + Warehouse & Items Seeds

--- db/migrate/20220613134543_create_items.rb
+++ db/migrate/20220613134543_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
       t.string :name
-      t.string :type_of_item
+      t.integer :type_of_item
       t.references :warehouse, null: false, foreign_key: true
 
       t.timestamps
